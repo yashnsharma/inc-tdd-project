8da750ffca6bf20b16df548b31c3c69555b85623
Yash Sharma <lostsharma@gmail.com>
2022-04-19 16:00:13 +0530

    Added more statements for running calculator

---
+++ index.js
@@ -0,0 +1,27 @@
+const add = require("./src/add");
+
+// *** Uncomment line to run  ***
+
+// * Run Add function for Empty Parameter
+// console.log(add(""));
+
+// * Run Add function for Single Parameter
+// console.log(add("123"));
+
+// * Run Add function for 2 Parameters
+// console.log(add("4,7"));
+
+// * Run Add function for 2 Parameters with 1 Negative
+// console.log(add("-4,7"));
+
+// * Run Add function for 2 Parameters with Newline string
+// console.log(add("\n4,7"));
+
+// * Run Add function for 2 Parameters with custom Delimiter
+// console.log(add("//;\n1;2"));
+
+// * Run Add function for 3 Parameters with custom Delimiter
+// console.log(add("//;\n1;2;3"));
+
+// * Run Add function for 15 Parameters with custom Delimiter
+// console.log(add("//;\n0;1;2;3;4;5;6;7;8;9;10;11;12;13;14"));
